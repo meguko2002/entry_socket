937d047e086a7dd0076b83a1ed858c2c5576a5b5
meguko <meguko2002@gmail.com>
2022-01-09 09:37:54 +0900

    排他的にエントリーボタンイネーブルできた

--- app.py
+++ app.py
@@ -1,15 +1,70 @@
-from flask import Flask, render_template, jsonify
-from flask_socketio import SocketIO, emit, join_room, leave_room
-import json
+from flask import Flask, render_template, jsonify, session, request
+from flask_socketio import SocketIO, emit, leave_room, join_room, send
+import random
 
 app = Flask(__name__)
 app.secret_key = 'ABCDEFGH'
 socketio = SocketIO(app)
 
-players = [{"id": 0, "name": 'AAA', 'cast': '', "isActive": False},
-           {"id": 1, "name": 'BBB', 'cast': '', "isActive": False},
-           {"id": 2, "name": 'CCC', 'cast': '', "isActive": False},
-           {"id": 3, "name": 'DDD', 'cast': '', "isActive": False}]
+players = [{'index': 0, 'name': '恵', "isActive": False, "sid": '', "isAlive": True, "isGM": False},
+           {'index': 1, 'name': 'Koji', "isActive": False, "sid": '', "isAlive": True, "isGM": False},
+           {'index': 2, 'name': '裕一', "isActive": False, "sid": '', "isAlive": True, "isGM": False}
+           ]
+
+
+class Cast:
+    pass
+
+
+class Villager(Cast):
+    def __init__(self):
+        self.name = '市民'
+        self.team = 'white'
+        self.color = 'white'
+
+
+class Werewolf(Cast):
+    def __init__(self):
+        self.name = '人狼'
+        self.team = 'black'
+        self.color = 'black'
+
+
+class FortuneTeller(Cast):
+    def __init__(self):
+        self.name = '占い師'
+        self.team = 'white'
+        self.color = 'white'
+
+
+villager = Villager()
+wolf = Werewolf()
+fortuneTeller = FortuneTeller()
+casts = [villager, wolf, fortuneTeller]
+
+
+class Village:
+    def __init__(self, players):
+        self.players = players
+        self.num = len(players)
+
+    # キャスト決め
+    def select_cast(self, casts):
+        pass
+
+    # キャストの割り当て
+    def assign_cast(self, casts):
+        for player, cast in zip(self.players, random.sample(casts, self.num)):
+            player["cast"] = cast.name
+
+    def is_assigned(self, sid):
+        for player in self.players:
+            if player['sid']==sid:
+                return player
+        return ''
+
+
+vil = Village(players)
 
 
 @app.route('/')
@@ -19,38 +74,48 @@ def index():
 
 @app.route('/player_list')
 def show_list():
-    return jsonify(players)
-
-
-@socketio.on('chat message')
-def handle_message(data):
-    message = 'received message: ' + data
-    print(message)
-    emit('server_message', 'form server ' + message, broadcast=True)
+    return jsonify(vil.players)
 
 
 @socketio.on('join')
-def on_join(json):
-    global players
-    players = json
-    print(str(players))
-    emit('entry room', players, broadcast=True)
+def join(index, isActive):
+    player = vil.players[index]
+    player['sid'] = request.sid
+    player['isActive'] = isActive
+    print(vil.players)
+    # room = player.get('cast')
+    # join_room(room)
+    emit('broadcast_message', {'players': vil.players}, broadcast=True)
 
 
-# @socketio.on('join')
-# def on_join(data):
-#     username = data['username']
-#     room = data['room']
-#     join_room(room)
-#     emit('server_message', username + ' さんは ' + room + ' に入室しました', to=room)
+# @socketio.on('candindexateGM')
+# def candidateGM(index):
+#     for pl in vil.players:
+#         pl["isGM"] = False
+#     player = vil.players[index]
+#     player["isGM"] = True
+#     emit('broadcast_message', {'msg': f'{player["name"]}さんはGMです', 'players': vil.players}, broadcast=True)
 
 
-@socketio.on('leave')
-def on_leave(data):
-    username = data['username']
-    room = data['room']
-    leave_room(room)
-    emit('server_message', username + ' さんは ' + room + ' から退出しました', to=room)
+@socketio.on('casting')
+def casting():
+    vil.assign_cast(casts)
+    emit('broadcast_message', {'players': vil.players}, broadcast=True)
+
+sids=[]
+@socketio.on("connect")
+def connect():
+    sid = request.sid
+    sids.append(sid)
+    player = vil.is_assigned(sid)
+    emit('login', {'data': player}, room=sid)
+
+
+
+@socketio.on("disconnect")
+def disconnect():
+    sid = request.sid
+    sids.remove(sid)
 
 
 if __name__ == '__main__':
---
+++ jinro.py
@@ -0,0 +1,15 @@
+
+players = [{"id": 0, "name": 'AAA', 'cast': '', "isActive": False},
+           {"id": 1, "name": 'BBB', 'cast': '', "isActive": False},
+           {"id": 2, "name": 'CCC', 'cast': '', "isActive": False},
+           {"id": 3, "name": 'DDD', 'cast': '', "isActive": False}]
+
+casts = [{'name':'werewolf', 'team':'black', 'color':'black'},
+         {'name':'villager', 'team':'white', 'color':'white'},
+         {'name':'fortuneTaller', 'team':'white', 'color':'white'},]
+
+class Village:
+    def __init__(self):
+        self.players = players
+
+
