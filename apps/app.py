0acf23be34aa27cb39524b15c510d8c00dd18492
meguko <meguko2002@gmail.com>
2022-06-09 23:00:04 +0900

    P140　テンプレートの継承、configの設定ファイル作成

--- apps/app.py
+++ apps/app.py
@@ -3,22 +3,24 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
-    app.config.from_mapping(
-        SECRET_KEY="asvmasASFewaasdEWAF",
-        SQLALCHEMY_DATABASE_URI="sqlite:///"+
-                                str(Path(Path(__file__).parent.parent, 'local.sqlite')),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO=True,
-        WTF_CSRF_SECRET_KEY="AfaseafasFAEfsadf",
-    )
+    # app.config.from_mapping(
+    #     SECRET_KEY="asvmasASFewaasdEWAF",
+    #     SQLALCHEMY_DATABASE_URI="sqlite:///"+
+    #                             str(Path(Path(__file__).parent.parent, 'local.sqlite')),
+    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
+    #     SQLALCHEMY_ECHO=True,
+    #     WTF_CSRF_SECRET_KEY="AfaseafasFAEfsadf",
+    # )
+    app.config.from_object(config[config_key])
     db.init_app(app)
     Migrate(app,db)
     csrf.init_app(app)
