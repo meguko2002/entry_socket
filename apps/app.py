6b6be60b8078157b90ee1faa34ef5033329115f9
meguko <meguko2002@gmail.com>
2022-06-11 19:32:52 +0900

    P153 signup まで確認

--- apps/app.py
+++ apps/app.py
@@ -4,28 +4,24 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from apps.config import config
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_login import LoginManager
 
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+login_manager.login_message = ""
 
 
 def create_app(config_key):
     app = Flask(__name__)
-    # app.config.from_mapping(
-    #     SECRET_KEY="asvmasASFewaasdEWAF",
-    #     SQLALCHEMY_DATABASE_URI="sqlite:///"+
-    #                             str(Path(Path(__file__).parent.parent, 'local.sqlite')),
-    #     SQLALCHEMY_TRACK_MODIFICATIONS=False,
-    #     SQLALCHEMY_ECHO=True,
-    #     WTF_CSRF_SECRET_KEY="AfaseafasFAEfsadf",
-    # )
     app.config.from_object(config[config_key])
     db.init_app(app)
     Migrate(app,db)
     csrf.init_app(app)
     toolbar = DebugToolbarExtension(app)
-
+    login_manager.init_app(app)
 
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
--- apps/auth/views.py
+++ apps/auth/views.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
-
+from flask import Blueprint, render_template, redirect, url_for, flash, request
+from apps.app import db
+from apps.crud.models import User
+from apps.auth.forms import SignUpForm
+from flask_login import login_user
 
 auth = Blueprint(
     "auth",
@@ -9,6 +12,30 @@ auth = Blueprint(
 )
 
 
-@auth.route("/")
+@auth.route('/')
 def index():
     return render_template('auth/index.html')
+
+
+@auth.route("/signup", methods=["POST","GET"])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data,
+        )
+        if user.is_duplicate_email():
+            flash("指定のメールアドレスは登録済みです")
+            return redirect(url_for("auth.signup"))
+
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)    # user情報をsessionに格納する
+
+        next_ = request.args.get("next")
+        if next_ is None or not next_.startswith("/"):
+            next_ = url_for("crud.users")
+        return redirect(next_)
+    return render_template('auth/signup.html', form=form)
